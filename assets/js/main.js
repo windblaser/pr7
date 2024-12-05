fe0fb735ed2f7cb299df97154fa3bfa6793bfe55
windblaser <ranelsova@gmail.com>
2024-12-06 01:05:50 +0300

    Пр7

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+btn.addEventListener('mousedown', function() {
+    btn.style.display = 'none';
+})
+btn.addEventListener('mouseup', function() {
+    btn.style.display = 'block';
+})
+btn2.addEventListener('mousedown', function(){
+    text.style.display = 'none';
+})
+btn2.addEventListener('mouseup', function(){
+    text.style.display = 'block';
+})
